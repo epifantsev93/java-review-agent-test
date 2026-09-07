618d56ea1466bdddbe166945fc8be56f444d2fb4
epifantsev <epifantsev93@gmail.com>
2026-09-07 13:00:16 +0300

    add PMD

--- src/main/java/agent/JavaReviewAgentApplication.java
+++ src/main/java/agent/JavaReviewAgentApplication.java
@@ -2,13 +2,13 @@ package agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JavaReviewAgentApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(JavaReviewAgentApplication.class, args);
+        SpringApplication.run(JavaReviewAgentApplication.class, args);
+//        ApplicationContext context = SpringApplication.run(JavaReviewAgentApplication.class, args);
 //        AIController controller = context.getBean(AIController.class);
 //        System.out.println(controller.generation("What is Java HashMap?"));
     }
